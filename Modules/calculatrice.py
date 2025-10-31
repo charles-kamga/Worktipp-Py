17ebc1ce3af791e4821df3d90e1c91048fb93503
charles-kamga <charleskamgapr@gmail.com>
2025-10-31 11:14:16 +0100

    essaie d'ajout de l'operation racine caree, et ajout des oppertions de puissance et modulo.

--- Modules/calculatrice.py
+++ Modules/calculatrice.py
@@ -1,3 +1,6 @@
+import math as mt
+
+
 def addition(a, b):
     return a + b
 
@@ -15,22 +18,43 @@ def division(a, b):
         return a / b
     return "Division par 0 impossible"
 
+
+def puissance(a,b):
+    if b == 0:
+        return 1
+    else:
+        return a**b
+
+def racine(a):
+    return mt.sqrt(a)
+
+
+def modulo(a,b):
+    if b == 0:
+        return "division par 0 impossible"
+    else:
+        return a%b
+
 #commentaire pour tester le push via GNU Pycharm
 while True:
     print("--------------------------------")
     print("Calculatrice simple : ")
-    print("1. Acalculatriceddition")
+    print("1. Addition")
     print("2. Soustraction")
     print("3. Multiplication")
     print("4. Division")
-    print("5.Quiter")
+    print("5. Puissance")
+    print("6. Racine")
+    print("7. Modulo")
+    print("8. Quitter")
     print("--------------------------------")
 
     choix = int(input("choisissez votre opperation : "))
 
-    if choix == 5:
+    if choix == 8:
         print("Fermeture de la calculatrice !")
         break
+
 
     nb1 = float(input("Entrer le premier nombre : "))
     nb2 = float(input("Entrer le deuxieme nombre : "))
@@ -45,3 +69,13 @@ while True:
 
     elif choix == 4:
         print(f"{nb2}/{nb1} = {division(nb1, nb2)}")
+
+    elif choix == 5:
+        print(f"{nb1}^{nb2} = {puissance(nb1,nb2)}")
+
+    elif choix == 7:
+        print(f"{nb1}={modulo(nb1,nb2)}[{nb2}]")
+
+    elif choix == 6:
+        nb1 = float(input("Entrer le nombre : "))
+        print(f"{nb1}½ = {racine(nb1)}")
